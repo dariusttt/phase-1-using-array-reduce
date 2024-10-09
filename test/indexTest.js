abc82345256c32615b785b00a3803250e8fa689b
dariuskemei <dariuskimutai76@gmail.com>
2024-10-09 21:45:50 +0300

    add index.js

--- test/indexTest.js
+++ test/indexTest.js
@@ -8,9 +8,17 @@ describe('reducer', function() {
       expect(totalBatteries).to.exist;
     });
 
+
+
+
     it('should have a number as a result', function() {
       expect(totalBatteries).to.be.a('number');
     });
+
+
+
+
+    
 
     it('should have made the sum of all the assembled batteries', function() {
       expect(totalBatteries).to.eql(31);
